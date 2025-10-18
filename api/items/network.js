391ae855574ab1fb2ad879fed0f16ce4b80c4e33
samuelorozco641 <samuel.orozco641@pascualbravo.edu.co>
2025-10-18 12:59:07 +0000

    Se agregan demas funciones

--- api/items/network.js
+++ api/items/network.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const response = require('../../network/response')
 const router = Router();
 const ctrl = require('./index');
-const {tiMonth, fuelEnergySelector, electricalConsumption, costElectricalKM, combustionConsumption, fuelConsumption, fuelEfficiency, fuelCostKm} = require('../../calculators/environment')
+const {tiMonth, fuelEnergySelector, electricalConsumption, costElectricalKM, combustionConsumption, fuelConsumption, fuelEfficiency, fuelCostKm, energyKm, emisionKm, savedEnergy, avoidedEmissions, monthlySavings, annualSavings, youngTree, oldTree, energyH2Cylinders, energyH2LowPresure, energyConsumed, hydrogenMass, litersRequired} = require('../../calculators/environment')
 
 const tableInjected = 'my_table'
 
@@ -20,6 +20,9 @@ router.post('/hello/:id', async (req, res) => {
 
 router.get('/environment/:ipc/:fuel', async (req, res) => {
     try {
+        //youngtree = 10
+        //oldtree = 30
+        //cellfuel = 0.54
         let list = {}
         const ipc = tiMonth(parseFloat(req.params.ipc))
         const fes = fuelEnergySelector(req.params.fuel)
@@ -29,6 +32,19 @@ router.get('/environment/:ipc/:fuel', async (req, res) => {
         const fuel_consumption = fuelConsumption(combustion, 12)
         const fuel_efficiency = fuelEfficiency(fuel_consumption)
         const fuel_cost_km = fuelCostKm(12, fuel_consumption)
+        const energy_km = energyKm(combustion)
+        const emision_km = emisionKm(12, energy_km)
+        const saved_energy = savedEnergy(combustion, electrical_consumption, 12)
+        const avoided_emissions = avoidedEmissions(emision_km, 12)
+        const monthly_savings = monthlySavings(fuel_cost_km, costElectrical_Km, 12)
+        const annual_savings = annualSavings(monthly_savings, ipc)
+        const young_tree = youngTree(avoided_emissions)
+        const old_tree = oldTree(avoided_emissions)
+        const energy_H2_Cylinders = energyH2Cylinders(10)
+        const energy_H2_Low_Presure = energyH2LowPresure(10)
+        const energy_consumed = energyConsumed(energy_H2_Low_Presure)
+        const hydrogen_mass = hydrogenMass(energy_H2_Low_Presure)
+        const liters_required = litersRequired(hydrogen_mass)
 
         list["it_month"] = ipc
         list["fuel_energy_selector"] = fes
@@ -38,6 +54,19 @@ router.get('/environment/:ipc/:fuel', async (req, res) => {
         list["fuel_consumption"] = fuel_consumption
         list["fuel_efficency"] = fuel_efficiency
         list["fuel_cost_km"] = fuel_cost_km
+        list["energy_km"] = energy_km 
+        list["emision_km"] = emision_km 
+        list["saved_energy"] = saved_energy
+        list["avoided_emissions"] = avoided_emissions
+        list["monthly_saving"] = monthly_savings
+        list["annual_savings"] = annual_savings
+        list["young_tree"] = young_tree
+        list["old_tree"] = old_tree
+        list["energy_H2_Cylinders"] = energy_H2_Cylinders
+        list["energy_H2_low_pressure"] = energy_H2_Low_Presure
+        list["energy_consumed"] = energy_consumed
+        list["hydrogen_mass"] = hydrogen_mass
+        list["liters_required"] = liters_required
 
         response.success(req, res, list, 200)
     } catch (error) {
